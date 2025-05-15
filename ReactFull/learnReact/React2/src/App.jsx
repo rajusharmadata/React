0c29dd3bd44aa444957eed62da9bbc09ab7ac29f
Raju kumar <rajusharmadata123@gmail.com>
2025-05-15 18:38:23 +0530

    add

---
+++ ReactFull/learnReact/React2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Button from './components/Button'
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <div>
+  <Navbar/>
+  <Card/>
+  <Button/>
+
+    </div>
+  )
+}
